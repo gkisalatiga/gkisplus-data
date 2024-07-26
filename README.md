a428df427cbb2f4e355af222dcb18de8b5390f4b
groaking <93555329+groaking@users.noreply.github.com>
2024-07-26 22:22:56 +0700

    Added automation for "Kebaktian Umum"

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Regularly auto-updated cloud data for the GKI Salatiga Plus app
 
 ## Update interval
 
+- **Kebaktian Umum (Regular Indonesian Service)**: every Saturday at 12 PM and 6 PM; every Sunday at 12 AM, 3 AM, and 6 AM
 - **SaRen Pagi (Morning Devotion)**: every day at 3 AM and 9 PM
 - **Tata Ibadah (Service Liturgy)**: every Saturday at 3 PM, 6 PM, and 9 PM; every Sunday at 12 AM and 3 AM
 - **Warta Jemaat (Church News)**: every Saturday at 3 PM, 6 PM, and 9 PM; every Sunday at 12 AM and 3 AM
---
+++ scripts/kebaktian_umum.py
@@ -0,0 +1,117 @@
+from datetime import datetime as dt
+from lxml import etree
+from lxml import html
+import json
+import os
+import requests as rq
+import time
+
+s = rq.Session()
+
+# The YouTube v3 API key.
+api_key = "${{ secrets.YT_V3_APIKEY }}"
+
+def get_data():
+    # Preamble logging.
+    print('Beginning the automation script for updating data: Kebaktian Umum')
+    t = dt.now()
+    
+    # GKI Salatiga channel's "Kebaktian Umum" playlist ID
+    playlistId = 'PLtAv1OZRTdvI1P3YIJ4_qOqapZjV1PtnI'
+    
+    # Preparing the request queries.
+    part = 'snippet'
+    maxResults = 20
+    
+    # The Google API v3 bridge.
+    bridge = 'https://www.googleapis.com/youtube/v3/playlistItems'
+    
+    # SOURCE: https://stackoverflow.com/a/55207539
+    print(f'Getting the video data for playlist: {playlistId} ...')
+    r = rq.get(bridge + f'?key={api_key}&part={part}&playlistId={playlistId}&maxResults={maxResults}')
+    
+    # The scraped YT video titles.
+    s1 = []
+    
+    # The scraped YT video ID.
+    s2 = []
+    
+    # The scraped YT upload date.
+    s3 = []
+    
+    # The scraped YT video description.
+    s4 = []
+    
+    # The scraped YT video URL.
+    s5 = []
+    
+    # The scraped YT video thumbnail.
+    s6 = []
+    
+    # Parsing the response JSON string.
+    i = 0
+    for a in r.json()['items']:
+        videoId = a['snippet']['resourceId']['videoId']
+        print(f'--- Iteration {i}; obtaining metadata for video ID: {videoId}')
+        
+        s1.append(a['snippet']['title'])
+        s2.append(videoId)
+        s3.append(a['snippet']['publishedAt'].split('T')[0])
+        s4.append(a['snippet']['description'].strip())
+        s5.append(f'https://www.youtube.com/watch?v={videoId}')
+        s6.append(a['snippet']['thumbnails']['high']['url'])
+        
+        i += 1
+    
+    # Parsing the current JSON file.
+    json_path = 'gkisplus.json'
+    fi = open(json_path, 'r')
+    j = json.load(fi)
+    fi.close()
+    
+    # Incrementing the update count and updating the 'last update' item.
+    # SOURCE: https://stackoverflow.com/a/27914405
+    j['meta']['update-count'] += 1
+    j['meta']['last-update'] = int( time.mktime(dt.now().timetuple()) )
+    j['meta']['last-updated-item'] = 'yt-video/umum'
+    
+    # Resetting the JSON node.
+    j['data']['yt-video']['umum'] = []
+    # Assumes identical s1, s2 and s3 list size.
+    # Writes the scraped data into the JSON file.
+    print('Morphing the JSON data ...')
+    for i in range(len(s1)):
+        print(f'Writing data no. {i}: {s1[i]}')
+        j['data']['yt-video']['umum'].append({
+            'title': s1[i],
+            'date': s3[i],
+            'desc': s4[i],
+            'link': s5[i],
+            'thumbnail': s6[i],
+            'is_live': 1
+        })
+    
+    # Writing into the remote GitHub repo's file.
+    # SOURCE: https://www.perplexity.ai/search/show-me-how-to-write-into-gith-YSsKLQ9wTGun0NGHscbNzw
+    # SOURCE: https://stackoverflow.com/a/12309296
+    print('Writing the JSON file ...')
+    with open(json_path, 'w') as fo:
+        json.dump(j, fo, ensure_ascii=False, indent=4)
+        fo.close()
+    
+    # Logging out.
+    print('-' * 25)
+    print('Finished the automation!')
+    print(f'Total script time: {dt.now() - t}')
+    
+if __name__ == "__main__":
+    # Added fail-safe for internet lost.
+    max_tries = 50
+    wait_timeout = 2
+    for i in range(max_tries):
+        try:
+            get_data()
+            break
+        except:
+            time.sleep(wait_timeout)
+            continue
