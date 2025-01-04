c30340776faea7d257443b06f7664bc3a3249007
groaking <93555329+groaking@users.noreply.github.com>
2025-01-05 05:48:59 +0700

    Now using mirror instead of direct update

---
+++ scripts-defer/mirror_gkisplus-data-json.py
@@ -0,0 +1,40 @@
+import json
+import requests as rq
+import sys
+
+
+prefix_local = 'v2/data/'
+prefix_remote = 'https://raw.githubusercontent.com/gkisalatiga/gkisplus-data-json/main/v2/data/'
+sources = [
+    'feeds.json',
+    'feeds.min.json',
+    'gkisplus-gallery.json',
+    'gkisplus-gallery.min.json',
+    'gkisplus-main.json',
+    'gkisplus-main.min.json',
+    'gkisplus-modules.json',
+    'gkisplus-modules.min.json',
+    'gkisplus-static.json',
+    'gkisplus-static.min.json',
+]
+
+exit_code_zero = False
+for l in sources:
+    # Remote change.
+    r = rq.get(prefix_remote + l)
+    c = r.content.decode()
+    j = json.loads(c)
+    
+    # Local file.
+    with open(prefix_local + l, 'r') as fi:
+        j_loc = json.load(fi)
+    
+    # Applying change/difference.
+    print(f'Mirroring {l}: {j_loc != j}')
+    if j_loc != j:
+        exit_code_zero = True
+        with open(prefix_local + l, 'w') as fo:
+            json.dump(j, fo)
+    
+print(f'Exit with code zero: {exit_code_zero}')
+sys.exit(0 if exit_code_zero else 234)
